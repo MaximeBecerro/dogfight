5abef3391c55d98ffed5c38f9ff24291bc9084ff
Max Becerro <Max Becerro@DESKTOP-BEP008O>
2018-04-24 10:22:59 +0200

    Test again

---
+++ src/jpu2016/dogfight/controller/DogfightController.java
@@ -0,0 +1,20 @@
+package jpu2016.dogfight.controller;
+
+public class DogfightController implements IOrderPerformer{
+	private int TIME_SLEEP = 30;
+	
+	DogfightController(IDogfightModel dogfightModel){
+		
+	}
+	
+	public void play() {}
+	
+	
+	
+	@Override
+	public void orderPerform(UserOrder userorder) {
+
+		
+	}
+	
+}
---
+++ src/jpu2016/dogfight/controller/UserOrder.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.controller;
+
+public class UserOrder implements IUserOrder{
+	private int player;
+	
+	public UserOrder(int player, Order order) {
+		
+	}
+	
+	@Override
+	public void getOrder(Order order) {
+
+		
+	}
+
+	@Override
+	public void getPlayer(int player) {
+
+		
+	}
+
+}
