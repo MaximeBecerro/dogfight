251d323132d6363d60ff0fd64b2faef479816d4a
Tristan-Guerro <33086546+Tristan-Guerro@users.noreply.github.com>
2018-04-24 11:06:01 +0200

    yyhrtj;:i_k y

--- src/jpu2016/dogfight/model/IArea.java
+++ src/jpu2016/dogfight/model/IArea.java
@@ -1,6 +1,5 @@
 package jpu2016.dogfight.model;
 
-
 public interface IArea {
 
 	public Dimension getDimension();
--- src/jpu2016/dogfight/model/IMobile.java
+++ src/jpu2016/dogfight/model/IMobile.java
@@ -1,6 +1,5 @@
 package jpu2016.dogfight.model;
 
-
 public interface IMobile {
 
 	public Direction getDirection();
