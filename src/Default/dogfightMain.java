bce7543375ab95fdf2382aa7cfe5297c069f6eb0
Max Becerro <Max Becerro@DESKTOP-BEP008O>
2018-04-24 14:29:14 +0200

    Update main

--- src/Default/dogfightMain.java
+++ src/Default/dogfightMain.java
@@ -5,7 +5,10 @@ import jpu2016.dogfight.controller.DogfightController;
 public class dogfightMain {
 
 	public static void main(String[] args) {
-		final DogfightModel dogfightModel = new DogfightModel(); final DogfightController dogfightController = new DogfightController(dogfightModel); final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel); dogfightController.setViewSystem(dogfightView); dogfightController.play(); 
+		final DogfightModel dogfightModel = new DogfightModel();
+		final DogfightController dogfightController = new DogfightController(dogfightModel);
+		final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
+		dogfightController.setViewSystem(dogfightView); dogfightController.play();
 	}
 
 }
