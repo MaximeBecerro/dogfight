7878b00c849cffb321d873647ffba18fe426c5a7
Lastpiece <33093886+Lastpiece@users.noreply.github.com>
2018-04-24 15:05:10 +0200

    phase 3

---
+++ src/jpu2016/dogfight/view/DogfightView.java
@@ -0,0 +1,21 @@
+package jpu2016.dogfight.view;
+
+import java.util.Observable;
+
+import jpu2016.dogfight.controller.IOrderPerformer;
+import jpu2016.dogfight.model.IDogfightModel;
+
+public class DogfightView implements IVewSystem {
+	public void DogfightView (IOrderPerformer orderPerformer, IDogfightModel dogfightModel, Observable observable) {
+		
+	}
+	private void run() {
+		
+	}
+	public void displayMessage (String message) {
+		
+	}
+	public void closeAll() {
+		
+	}
+}
---
+++ src/jpu2016/gameframe/GameFrame.java
@@ -0,0 +1,19 @@
+package jpu2016.gameframe;
+
+import java.awt.event.KeyEvent;
+import java.util.Observable;
+
+public class GameFrame {
+	public void GameFrame (String title, IEventPerformer performer, IGraphicsBuilder graphicBuilder, Observable observable) {
+		
+	}
+	public void keyPressed(KeyEvent keyEvent) {
+		
+	}
+	public void keyReleased(KeyEvent keyEvent) {
+		
+	}
+	public void keyTyped(KeyEvent keyEvent) {
+		
+	}
+}
